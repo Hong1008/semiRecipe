f53ba2eff1744b89cde8275ed12728bdfe8c1533
j1su94 <j1su_94@hanmail.net>
2019-08-06 17:45:12 +0900

    로그인 페이지.. 카카오 로그인..

--- src/action/LoginAction.java
+++ src/action/LoginAction.java
@@ -13,10 +13,10 @@ import model.MemberDTO;
 public class LoginAction {
 	public void execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
 		MemberDTO dto = new MemberDTO();
-		dto.setUser_id(req.getParameter("id"));
-		dto.setUser_pw(req.getParameter("pw"));
+		dto.setUser_id(req.getParameter("user_id"));
+		dto.setUser_pw(req.getParameter("user_pw"));
 		
-		System.out.println(req.getParameter("id"));
+		System.out.println("여기까진 오나요");
 		
 		MemberDAO dao = MemberDAO.getInstance();
 		int cnt = dao.login(dto);
@@ -28,8 +28,10 @@ public class LoginAction {
 		HttpSession session = null;
 		if(cnt==1) {	// 회원일 때
 			session = req.getSession();
-			session.setAttribute("loginID", req.getParameter("id")); 	// loginID라는 이름으로 id 값을 세션에 저장
+			session.setAttribute("loginID", req.getParameter("user_id")); 	// loginID라는 이름으로 id 값을 세션에 저장
 			session.setMaxInactiveInterval(30*60); 	// 30분
+		}else {
+			System.out.println("실패!");
 		}
 		
 	} // end execute()
