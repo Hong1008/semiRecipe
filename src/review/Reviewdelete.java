db63a026e848dee45d51bd582b1264e2a6457cd7
zxcbn <zxcbn@DESKTOP-S6NDNO7>
2019-08-16 03:02:41 +0900

    패키지 변경

---
+++ src/review/Reviewdelete.java
@@ -0,0 +1,17 @@
+package review;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import review.model.ReviewDAO;
+
+public class Reviewdelete {
+	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		ReviewDAO dao = new ReviewDAO();
+		
+		System.out.println(req.getParameter("review_num"));
+		int review_num = Integer.parseInt(req.getParameter("review_num"));
+		dao.deleteMethod(review_num);
+	}
+
+}
